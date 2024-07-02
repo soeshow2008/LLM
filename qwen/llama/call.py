8aa7a729a931a59111aaebdad109adefb44b2315
gujinji <gujinji@MBP-2G523WMD6P-0036.local>
2024-07-02 22:18:17 +0800

    llama.cpp

--- qwen/llama/call.py
+++ qwen/llama/call.py
@@ -1,4 +1,7 @@
 import openai
+import sys
+
+prompt = sys.argv[1]
 
 client = openai.OpenAI(
     base_url="http://localhost:8080/v1", # "http://<Your api-server IP>:port"
@@ -9,7 +12,7 @@ completion = client.chat.completions.create(
     model="qwen",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "tell me something about michael jordan"}
+        {"role": "user", "content": prompt}
     ]
 )
 print(completion.choices[0].message.content)
--- qwen/readme.txt
+++ qwen/readme.txt
@@ -1,2 +1,3 @@
 1.https://github.com/QwenLM
+2.cn:https://qwen.readthedocs.io/zh-cn/latest/
 
